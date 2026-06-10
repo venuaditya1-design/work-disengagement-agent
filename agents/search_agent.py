c94c5b63b5925f90e29f8543b59b75dd9254e842
venuaditya1-design <venu.aditya1@gmail.com>
2026-06-10 23:40:44 +0530

    Update search_agent.py

--- agents/search_agent.py
+++ agents/search_agent.py
@@ -13,6 +13,7 @@ KEYWORDS = [
     "burnout disengagement"
 ]
 
+
 def search_openalex(keyword, per_page=50):
 
     params = {
@@ -23,6 +24,7 @@ def search_openalex(keyword, per_page=50):
     response = requests.get(BASE_URL, params=params)
 
     if response.status_code != 200:
+        print(f"Error searching {keyword}")
         return []
 
     data = response.json()
@@ -32,30 +34,108 @@ def search_openalex(keyword, per_page=50):
     for item in data.get("results", []):
 
         title = item.get("title", "")
+
         year = item.get("publication_year")
+
         doi = item.get("doi")
 
         journal = ""
 
         if item.get("primary_location"):
+
             source = item["primary_location"].get("source")
 
             if source:
-                journal = source.get("display_name", "")
+                journal = source.get(
+                    "display_name",
+                    ""
+                )
 
         authors = []
 
         for a in item.get("authorships", []):
+
             author = a.get("author", {})
-            authors.append(author.get("display_name", ""))
+
+            authors.append(
+                author.get(
+                    "display_name",
+                    ""
+                )
+            )
+
+        open_access = item.get(
+            "open_access",
+            {}
+        )
+
+        pdf_link = open_access.get(
+            "oa_url"
+        )
+
+        access_type = (
+            "Open Access"
+            if open_access.get(
+                "is_oa"
+            )
+            else "Premium"
+        )
+
+        pdf_available = (
+            "Yes"
+            if pdf_link
+            else "No"
+        )
+
+        landing_page = ""
+
+        if item.get("primary_location"):
+
+            landing_page = item[
+                "primary_location"
+            ].get(
+                "landing_page_url",
+                ""
+            )
 
         papers.append({
+
             "title": title,
-            "authors": ", ".join(authors),
+
+            "authors": ", ".join(
+                authors
+            ),
+
             "year": year,
+
             "doi": doi,
+
             "journal": journal,
+
+            "abstract": str(
+                item.get(
+                    "abstract_inverted_index",
+                    {}
+                )
+            ),
+
+            "citation_count": item.get(
+                "cited_by_count",
+                0
+            ),
+
+            "access_type": access_type,
+
+            "pdf_available": pdf_available,
+
+            "pdf_link": pdf_link,
+
+            "repository_link": landing_page,
+
+            "publisher_link": landing_page,
+
             "keyword": keyword,
+
             "source": "OpenAlex"
         })
 
