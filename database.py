c25b37cc426e82820843b2537682ef2e9cfa8aff
venuaditya1-design <venu.aditya1@gmail.com>
2026-06-10 18:19:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -7,24 +7,23 @@ def init_db():
 
     conn.execute("""
     CREATE TABLE IF NOT EXISTS papers (
-        conn.execute("""
-CREATE TABLE IF NOT EXISTS papers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT,
-    authors TEXT,
-    year INTEGER,
-    doi TEXT UNIQUE,
-    journal TEXT,
-    abstract TEXT,
-    citation_count INTEGER,
-    access_type TEXT,
-    pdf_available TEXT,
-    pdf_link TEXT,
-    repository_link TEXT,
-    publisher_link TEXT,
-    keyword TEXT,
-    source TEXT
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        authors TEXT,
+        year INTEGER,
+        doi TEXT UNIQUE,
+        journal TEXT,
+        abstract TEXT,
+        citation_count INTEGER,
+        access_type TEXT,
+        pdf_available TEXT,
+        pdf_link TEXT,
+        repository_link TEXT,
+        publisher_link TEXT,
+        keyword TEXT,
+        source TEXT
     )
     """)
+
     conn.commit()
     conn.close()
