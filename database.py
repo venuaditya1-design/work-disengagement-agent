c1f79ea4dd0f2476fa3979c13cc0173e62a7b9ac
venuaditya1-design <venu.aditya1@gmail.com>
2026-06-11 00:39:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,27 +3,42 @@ import sqlite3
 DB_NAME = "research.db"
 
 def init_db():
+
     conn = sqlite3.connect(DB_NAME)
 
     conn.execute("""
     CREATE TABLE IF NOT EXISTS papers (
+
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+
         title TEXT,
         authors TEXT,
         year INTEGER,
         doi TEXT UNIQUE,
         journal TEXT,
+
         abstract TEXT,
+
+        relevance_score INTEGER,
+
         citation_count INTEGER,
+
         access_type TEXT,
+
         pdf_available TEXT,
+
         pdf_link TEXT,
+
         repository_link TEXT,
+
         publisher_link TEXT,
+
         keyword TEXT,
+
         source TEXT
     )
     """)
 
     conn.commit()
+
     conn.close()
